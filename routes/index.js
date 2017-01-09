bfd5cbafbd2eb4a3fa1a5503df16793e1757134d
S K <xz64@users.noreply.github.com>
2017-01-09 15:43:06 +0000

    feat: add CSRF protection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var Router = require('koa-router');
+var HttpStatus = require('http-status-codes');
 
 var publicRouter = require('./public');
 var privateRouter = require('./private');
 
 var router = new Router();
+
+router.post('*', function* (next) {
+  if (this.request.headers['x-requested-with'] !== 'XMLHttpRequest') {
+    // csrf protection
+    this.response.status = HttpStatus.BAD_REQUEST;
+    this.body = { error: 'missing CSRF header (X-Requested-With: '
+      + 'XMLHttpRequest)' };
+    return;
+  }
+  yield next;
+});
 
 router.use('/api', publicRouter.routes(), publicRouter.allowedMethods());
 
