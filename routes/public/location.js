530e7d7b810f00af44777eb2525a41cf7b2fd255
S K <xz64@users.noreply.github.com>
2017-01-14 16:53:46 +0000

    feat: add flag to indicate if going to location

--- routes/public/location.js
+++ routes/public/location.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router');
 const HttpStatus = require('http-status-codes');
+const mongoose = require('mongoose');
 
+const Place = require('../../models/place');
 const googleMapsClient = require('../../googleMapsClient');
 
 const router = new Router();
@@ -12,12 +14,26 @@ router.get('/places', function* () {
     return;
   }
   const results = yield googleMapsClient.findRestaurants(loc);
+  const ids = results.map(function(result) {
+    return result.id;
+  });
+  const userId = this.req.user && this.req.user._id;
+  const userObjectId = new mongoose.Types.ObjectId(userId);
+  const existingPlaces = yield Place.find({
+    id: { $in: ids },
+    goers: userObjectId
+  }).exec();
+  const existingPlacesMap = {};
+  existingPlaces.forEach(function(place) {
+    existingPlacesMap[place.id] = true;
+  });
   this.body = results.map(function(result) {
     return {
       id: result.id,
       name: result.name,
       photo_reference: result.photos && result.photos[0].photo_reference,
-      description: result.vicinity
+      description: result.vicinity,
+      amGoing: !!existingPlacesMap[result.id]
     };
   });
 });
