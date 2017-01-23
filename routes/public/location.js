55af46a6edb28c9ac0144d892fa4a78213dfaabd
S K <xz64@users.noreply.github.com>
2017-01-23 20:47:51 +0000

    feat: add number of people going per route

--- routes/public/location.js
+++ routes/public/location.js
@@ -20,12 +20,23 @@ router.get('/places', function* () {
   const userId = this.req.user && this.req.user._id;
   const userObjectId = new mongoose.Types.ObjectId(userId);
   const existingPlaces = yield Place.find({
-    id: { $in: ids },
-    goers: userObjectId
+    id: { $in: ids }
   }).exec();
   const existingPlacesMap = {};
   existingPlaces.forEach(function(place) {
-    existingPlacesMap[place.id] = true;
+    var amGoing = false;
+
+    for (var i = 0; i < place.goers.length; i++) {
+      if (place.goers[i].equals(userObjectId)) {
+        amGoing = true;
+        break;
+      }
+    }
+
+    existingPlacesMap[place.id] = {
+      amGoing: amGoing,
+      numGoing: place.goers.length
+    };
   });
   this.body = results.map(function(result) {
     return {
@@ -33,7 +44,8 @@ router.get('/places', function* () {
       name: result.name,
       photo_reference: result.photos && result.photos[0].photo_reference,
       description: result.vicinity,
-      amGoing: !!existingPlacesMap[result.id]
+      amGoing: (existingPlacesMap[result.id] && existingPlacesMap[result.id].amGoing) || false,
+      numGoing: (existingPlacesMap[result.id] && existingPlacesMap[result.id].numGoing) || 0
     };
   });
 });
